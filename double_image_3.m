9bb1c566b80eab2beb03e06c09e110d9a00a5453
charlotte_win <1539009015@qq.com>
2024-11-01 21:58:04 +0800

    change decrypt

--- double_image_3.m
+++ double_image_3.m
@@ -2,7 +2,7 @@ clc
 clear
 
 % 分块大小
-bs = 5;
+bs = 3;
 
 % 嵌入数据生成
 num_D = 1000000;
@@ -25,7 +25,7 @@ double_I = [inter_I, inter_I];
 
 [encrypt_I, emd_D] = encrypt(double_I, bs, b, D, row, col);
 
-[recover_I, ext_D]= decrypt(encrypt_I, bs, b, row, col);
+[recover_I, ext_D]= new_decrypt(encrypt_I, bs, b, row, col);
 
 num_emd = length(emd_D);
 [MSE, PSNR, ER, BPP] = caulate(row, col, inter_I, recover_I, num_emd);
